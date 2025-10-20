87cfe5b888227da156668bdc53632b00372429e1
Jon Clarke <jonathan.clarke@holidayextras.com>
2025-10-20 21:44:18 +0100

    init

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,43 @@
+class ItemsController < ApplicationController
+  before_action :set_item, only: %i[show edit update]
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to @item
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to @item
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    def item_params
+      params.expect(item: [ :name, :quantity ])
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,29 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+  # get "/items", to: "items#index"
+  #
+  # get "/items/new", to: "items#new"
+  # post "/items", to: "items#create"
+  #
+  # get "/items/:id", to: "items#show"
+  #
+  # get "/items/:id/edit", to: "items#edit"
+  # patch "/items/:id", to: "items#update"
+  # put "/items/:id", to: "items#update"
+  #
+  # delete "/items/:id", to: "items#destroy"
+
+  root "items#index"
+  resources :items
+end
